055a25da0e69bc321a622c53a11cd71c51aec356
Khiem Pham <Khiem Pham>
2016-12-31 20:53:44 -0800

    infinitum 17

--- hackerrank/adinfinitum 17/untitled/src/divisor.java
+++ hackerrank/adinfinitum 17/untitled/src/divisor.java
@@ -10,6 +10,7 @@ public class divisor {
 
     //product *= (biggestTerm - (ithPrime - 1) * biggestPower - 1) / ((ithPrime - 1) * (ithPrime - 1))
     public static void main(String[] args) {
+        System.out.println(firstNPrimes[1000]);
         Scanner in = new Scanner(System.in);
         int q = in.nextInt();
         StringBuffer s = new StringBuffer();
@@ -18,17 +19,17 @@ public class divisor {
             int a = in.nextInt();
             long product = 1;
             for (int i = 1; i <= m; i++) {
-                int ithPrime = firstNPrimes[i];
-                int biggestPower = a + i + 2;
-                int biggestTerm = powerMod(ithPrime, biggestPower, mod)+mod;
-                int secondTerm = ((ithPrime-1)*biggestPower+1)%mod;
-                int square = (int)((long) (ithPrime - 1) * (long) (ithPrime - 1) % mod);
+                long ithPrime = (long)firstNPrimes[i];
+                long biggestPower = a + i + 2;
+                long biggestTerm = powerMod(ithPrime, biggestPower, mod)+mod;
+                long secondTerm = ((ithPrime-1)*biggestPower+1)%mod;
+                long square = ((ithPrime - 1)*(ithPrime - 1) % mod);
                 if (square == 1 || square == 0) {
                     product *= (biggestTerm-secondTerm)%mod;
                 }
                 else {
-                    int bottom = getInverseMod(square, mod) % mod;
-                    product *= (long)(biggestTerm - secondTerm) * (long)bottom % mod;
+                    long bottom = getInverseMod(square, mod) % mod;
+                    product *= (biggestTerm - secondTerm) * (bottom) % mod;
                     product %= mod;
                 }
             }
@@ -58,26 +59,26 @@ public class divisor {
         return primes;
     }
 
-    static private int powerMod(int i, int j, int k) {
+    static private long powerMod(long i, long j, long k) {
         if (j == 1) {
             return i % k;
         } else {
             long x = powerMod(i, j / 2, k);
             if (j % 2 == 1) {
-                return(int) ((x * x * i) % k);
+                return ((x * x * i) % k);
             } else {
-                return (int) ((x * x) % k);
+                return ((x * x) % k);
             }
         }
     }
-    public static int getInverseMod(int n, int mod) {
-        int big = (n <= mod) ? mod : n;
-        int small = (n<=mod) ? n : mod;
-        int r;
-        int q;
-        int s1 = 0;
-        int s2 = 1;
-        int s3;
+    public static long getInverseMod(long n, long mod) {
+        long big = (n <= mod) ? mod : n;
+        long small = (n<=mod) ? n : mod;
+        long r;
+        long q;
+        long s1 = 0;
+        long s2 = 1;
+        long s3;
         while ((r = big%small) !=0){
             q = big/small;
             s3 = s1 - s2*q;
@@ -87,7 +88,6 @@ public class divisor {
             small = r;
 
         }
-        if (small > 1) return -1;
         return s2 = (s2<0)? (s2+mod):s2;
     }
 }
