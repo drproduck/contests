2d702ea378d653924328319b8a392eb68c20be46
Khiem Pham <duckhiem95@yahoo.com>
2017-01-18 01:10:51 -0800

    av8

--- codeforces/practice/source/src/C.java
+++ codeforces/practice/source/src/C.java
@@ -8,48 +8,48 @@ public class C {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         RankedDisjointSets ds = new RankedDisjointSets(n);
-        for (int i = 1;i<=n;i++) {
-            ds.union(i , in.nextInt());
+        for (int i = 1; i <= n; i++) {
+            ds.union(i, in.nextInt());
         }
         System.out.println(ds.numTrees);
     }
-}
 
-class RankedDisjointSets{
-    int[] parent;
-    int[] rank;
-    int numTrees;
+    static class RankedDisjointSets {
+        int[] parent;
+        int[] rank;
+        int numTrees;
 
-    RankedDisjointSets(int size) {
-        parent = new int[size+1];
-        rank = new int[size+1];
-        numTrees = size;
-    }
-
-    void union(int x, int y) {
-        x = find(x);
-        y = find(y);
-
-        if (x == y) {
-            return;
+        RankedDisjointSets(int size) {
+            parent = new int[size + 1];
+            rank = new int[size + 1];
+            numTrees = size;
         }
-        else {
-            numTrees --;
-            if (rank[x] < rank[y]) {
-                parent[x] = y;
-            } else if (rank[x] > rank[y]){
-                parent[y] = x;
-            }
-            else {
-                parent[y] = x;
-                rank[x]++;
+
+        void union(int x, int y) {
+            x = find(x);
+            y = find(y);
+
+            if (x == y) {
+                return;
+            } else {
+                numTrees--;
+                if (rank[x] < rank[y]) {
+                    parent[x] = y;
+                } else if (rank[x] > rank[y]) {
+                    parent[y] = x;
+                } else {
+                    parent[y] = x;
+                    rank[x]++;
+                }
             }
         }
-    }
 
-    int find(int x) {
-        if (parent[x] == 0) {
-            return x;
-        } else return parent[x] = find(parent[x]);
+        int find(int x) {
+            if (parent[x] == 0) {
+                return x;
+            } else return parent[x] = find(parent[x]);
+        }
     }
 }
+
+
---
+++ codeforces/practice/source/src/D.java
@@ -0,0 +1,137 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Khiem on 1/17/2017.
+ */
+public class D {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        SegmentTree st = new SegmentTree(n);
+        int k = in.nextInt();
+        if (k > n / 2) {
+            k = n - k;
+        }
+        int t = 1;
+        int num = 1;
+        StringBuffer str = new StringBuffer();
+        do {
+            st.increment(t);
+            int t2 = (t+k)%n;
+            st.increment(t2);
+            int tt = (t+1)%n;
+            int tt2 = (t2 == 0) ? n - 1 : (t2 - 1) % n;
+            num = num + 1 + st.rsq(tt, tt2);
+            t = t2;
+            str.append(num + " ");
+        } while (t != 1);
+        System.out.println(Arrays.toString(st.a));
+        System.out.println(Arrays.toString(st.st));
+        System.out.println(str.toString());
+    }
+
+    static class SegmentTree {
+        int[] a;
+        int[] st;
+        int n;
+
+        /**
+         * everything is 0 in the beginning
+         * @param n
+         */
+        public SegmentTree(int n) {
+            a = new int[n];
+            st = new int[3*n];
+            this.n  = n ;
+            build(1, 0, n-1);
+        }
+
+        public SegmentTree(int n, int[] array) {
+            a = array;
+            this.n = n;
+            st = new int[4 * n];
+            build(1, 0, n - 1);
+        }
+        int left(int p) {
+            return p << 1;
+        }
+
+        int right(int p) {
+            return (p << 1) +1;
+        }
+
+        int[] st() {
+            return st;
+        }
+
+        void build(int p, int L, int R) {
+            for (int i = L; i <= R; i++) {
+                st[p] += a[i];
+            }
+            if (L == R) st[p] = a[L];
+            else {
+                build(left(p), L, (L + R) / 2);
+                build(right(p), (L + R) / 2 + 1, R);
+            }
+        }
+
+        int rsq(int i, int j) {
+            if (i<= j)
+            return rsq(1, 0, n - 1, i, j);
+            else if (i == j+1) return st[1];
+            else return st[1] - rsq(1, 0, n - 1, j+1, i-1);
+        }
+
+        int rsq(int p, int L, int R, int i, int j) {
+            if (i > R || j < L) {
+                return 0;
+            }
+            if (L >= i && R <= j) {
+                return st[p];
+            }
+            return rsq(left(p), L, (L + R) / 2, i, j) + rsq(right(p), (L + R) / 2 + 1, R,  i, j);
+        }
+
+        int update(int newVal, int pos) {
+            int oldVal = a[pos];
+            if (oldVal == newVal) return oldVal;
+            a[pos] = newVal;
+            int dif = newVal - oldVal;
+            int p = 1;
+            int L = 0;
+            int R = n-1;
+            while (L!=R) {
+                st[p] += dif;
+                if (pos < (L + R) / 2 + 1) {
+                    R = (L + R) / 2;
+                    p = p*2;
+                } else {
+                    L = (L + R) / 2+1;
+                    p = p*2+1;
+                }
+            }
+            return oldVal;
+        }
+
+                int increment(int pos) {
+                    int oldVal = a[pos];
+                    a[pos] ++;
+                    int dif = 1;
+                    int p = 1;
+                    int L = 0;
+                    int R = n-1;
+                    while (L!=R) {
+                st[p] += dif;
+                if (pos < (L + R) / 2 + 1) {
+                    R = (L + R) / 2;
+                    p = p*2;
+                } else {
+                    L = (L + R) / 2+1;
+                    p = p*2+1;
+                }
+            }
+            return oldVal;
+        }
+    }
+}
