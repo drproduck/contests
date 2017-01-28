5a51ccb23e7e96f1eb7412e8d3db0414cb4e4044
Khiem Pham <duckhiem95@yahoo.com>
2017-01-27 18:45:42 -0800

    linux ?

--- codeforces/392/source/src/E.java
+++ codeforces/392/source/src/E.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Khiem on 1/20/2017.
@@ -12,22 +9,26 @@ public class E {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedReader((new InputStreamReader(System.in))));
         int n = in.nextInt();
-        int[][] a = new int[n][5];
+        List<int[]>[] a = new ArrayList[n+1];
+        int[][] b = new int[n + 1][];
+        int[] inDeg = new int[n + 1];
         for (int i = 1; i <n;i++) {
-            for (int j = 0;j<4;j++) {
-                a[i][j] = in.nextInt();
+            int v = in.nextInt();
+            inDeg[v] ++;
+            int[] c = new int[3];
+            for (int j = 0;j<3;j++) {
+                c[j] = in.nextInt();
             }
-            a[i][4] = i;
+            if (a[v] == null) {
+                a[v] = new ArrayList<>();
+            }
+            a[v].add(c);
+            b[i] = c;
         }
-        Arrays.sort(a, (int[] x, int[] y) -> (x[0] - y[0]));
         System.out.println(Arrays.deepToString(a));
-        Set<Integer> leaves = new HashSet<>();
-        int[] bool = new int[n+1];
+        Queue<Integer> leaves = new ArrayDeque<>();
         for (int i = 1; i <= n; i++) {
-            bool[a[i][0]] =1;
-        }
-        for (int i = 1;i<n;i++) {
-            if (bool[i] == 0) {
+            if (a[i] == null) {
                 leaves.add(i);
             }
         }
